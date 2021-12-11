cf96207212b7cc51b7684f7b06f02271904f2c45
balajireddy <balajireddy@LAPTOP-R7I474I9>
2021-12-11 11:41:46 +0530

    project new

---
+++ TestYantra/src/main/java/Sample3_mavin_proj/TestYantra/App.java
@@ -0,0 +1,13 @@
+package Sample3_mavin_proj.TestYantra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
